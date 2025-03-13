2eaae56cee747dd9d83f775f2c8e1f8516cc3863
Md Ashiqur Rahman <joyashikur@gmail.com>
2025-03-13 22:17:29 +0100

    initial publication component

---
+++ src/components/core/publication/publications.tsx
@@ -0,0 +1,10 @@
+import { Flex } from "@radix-ui/themes"
+import { SectionHeading } from "../section-heading"
+
+export const Publications = () => {
+    return (
+        <Flex direction={'column'} gap={'5'}>
+            <SectionHeading title="Publications" />
+        </Flex>
+    )
+}
