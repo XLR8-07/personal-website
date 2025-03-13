ebaa15fe7d3975d25903d48ca520dabb05a7dc8c
Md Ashiqur Rahman <joyashikur@gmail.com>
2025-03-13 22:17:18 +0100

    page layout update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,19 +3,21 @@ import { Technology } from '@/components/core/about/technology';
 import { Education } from '@/components/core/education/education';
 import { Hero } from '@/components/core/hero/hero';
 import { Projects } from '@/components/core/projects/projects';
+import { Publications } from '@/components/core/publication/publications';
 import { References } from '@/components/core/references/references';
 import { Work } from '@/components/core/work/work';
 import { Flex } from '@radix-ui/themes';
 
 export default function Page() {
     return (
-        <Flex direction={ 'column' } className={ 'relative' } gap={ '8' }>
+        <Flex direction={'column'} className={'relative'} gap={'8'}>
             <Hero />
             <About />
             <Work />
             <Education />
-            <Technology />
+            <Publications />
             <Projects />
+            <Technology />
             <References />
         </Flex>
     );
